5ef831e29841ce54cd73080c319c8eb9075f6b96
IHCantabraba <inigocanta@gmail.com>
2024-11-06 19:57:59 +0100

    fixed ageValidation

--- src/utils/validateDateOfBIrth.js
+++ src/utils/validateDateOfBIrth.js
@@ -6,9 +6,9 @@ export const validateDateOfBirth = (date) => {
   console.log(date.slice(0, 4))
   console.log(currentYear - Number(date.slice(0, 4)))
 
-  if (!currentYear - Number(date.slice(0, 4)) > 16 && data === '') {
+  if (currentYear - Number(date.slice(0, 4)) >= 16) {
     valid = true
   }
-
+  console.log(`valid value is: ${valid}`)
   return valid
 }
